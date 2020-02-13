4b9a0746cfcf92036de23da4da2c9e2e094f47c0
Ditwan Price <price.ditwan@gmail.com>
2020-02-12 22:38:39 -0500

    Initial cart page setup.

--- Bookstore/shopping_cart/views.py
+++ Bookstore/shopping_cart/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def cart(request):
+    return HttpResponse('<h1>Bookstore Shopping Cart</h1>')
