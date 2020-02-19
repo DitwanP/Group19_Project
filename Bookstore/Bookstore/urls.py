9a09a6a0f1b436ebc027a13083bf68f2638e406f
jzhan053 <55008402+jzhan053@users.noreply.github.com>
2020-02-19 17:07:47 -0500

    Update urls.py

--- Bookstore/Bookstore/urls.py
+++ Bookstore/Bookstore/urls.py
@@ -15,7 +15,18 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from book_details import views
+from . import settings
+from django.contrib.staticfiles.urls import static
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+ 
+ 
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('book/<int:pk>', views.bookDetailsView.as_view(), name='book-details'),
 ]
+
+urlpatterns += staticfiles_urlpatterns()
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
