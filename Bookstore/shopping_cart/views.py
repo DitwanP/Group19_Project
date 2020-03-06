2c446bceb4555169dd0ce35cb94bc48303bc99b9
Ditwan Price <price.ditwan@gmail.com>
2020-03-06 02:47:59 -0500

    fallback

--- Bookstore/shopping_cart/views.py
+++ Bookstore/shopping_cart/views.py
@@ -1,9 +1,67 @@
-from django.shortcuts import render, get_object_or_404
-
 from .models import Cart_item
+
+from django.conf import settings
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from django.urls import reverse
+from django.shortcuts import render, redirect, get_object_or_404
+
+from profile_management.models import Profile
+from products.models import Product
+
+from shopping_cart.extras import generate_order_id
+from shopping_cart.models import OrderItem, Order, Transaction, Cart_item
+
+import datetime
 
 def cart(request):
 
     items = Cart_item.objects.all()
 
     return render(request, 'shopping_cart/cart.html', {'items' : items})
+
+def get_user_pending_order(request):
+    # get order for the correct user
+    user_profile = get_object_or_404(Profile, user=request.user)
+    order = Order.objects.filter(owner=user_profile, is_ordered=False)
+    if order.exists():
+        # get the only order in the list of filtered orders
+        return order[0]
+    return 0
+
+
+@login_required()
+def add_to_cart(request, **kwargs):
+
+    # filter products by id
+    book = get_object_or_404(Book, pk=book_id)
+
+    # create orderItem of the selected product
+    order_item, status = OrderItem.objects.get_or_create(product=product)
+
+    # show confirmation message and redirect back to the same page
+    messages.info(request, "item added to cart")
+    return redirect(reverse('products:product-list'))
+
+
+@login_required()
+def delete_from_cart(request, item_id):
+    item_to_delete = OrderItem.objects.filter(pk=item_id)
+    if item_to_delete.exists():
+        item_to_delete[0].delete()
+        messages.info(request, "Item has been deleted")
+    return redirect(reverse('shopping_cart:cart'))
+
+
+@login_required()
+def order_details(request, **kwargs):
+    existing_order = get_user_pending_order(request)
+    context = {
+        'order': existing_order
+    }
+    return render(request, 'shopping_cart/cart.html', context)
+
+
+def success(request, **kwargs):
+    # a view signifying the transcation was successful
+    return render(request, 'shopping_cart/purchase_success.html', {})
