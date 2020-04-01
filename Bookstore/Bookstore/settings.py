3a70ed71a33f2c6481c66a06d6fdbd21456d5cff
Ditwan Price <price.ditwan@gmail.com>
2020-04-01 16:19:10 -0400

    These pages now redirect to the actual homepage

--- Bookstore/Bookstore/settings.py
+++ Bookstore/Bookstore/settings.py
@@ -46,7 +46,8 @@ INSTALLED_APPS = [
     'shopping_cart',
     'profile_management',
     'book_details',
-    'book_browsing'
+    'book_browsing',
+]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
